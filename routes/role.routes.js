0b7b5fc0df1e6b8c4e9c05f45bf621c3f5f5663d
Gautier <hello.gautiermaire@gmail.com>
2022-04-12 21:49:20 +0200

    swagger for role routes

--- routes/role.routes.js
+++ routes/role.routes.js
@@ -7,3 +7,56 @@ router.get('/', controller.getRoles)
 router.get('/:id', controller.getRole)
 
 module.exports = router;
+
+/**
+ * @swagger
+ * tags:
+ *   name: Roles
+ *   description: Role retrieval
+ */
+
+/**
+ * @swagger
+ * /admin/roles:
+ *   get:
+ *     summary: Get all roles
+ *     tags: [Roles]
+ *     responses:
+ *       "302":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 results:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Role'
+ *       "500":
+ *         $ref: '#/components/responses/Forbidden'
+ */
+
+/**
+ * @swagger
+ * /admin/roles/{id}:
+ *   get:
+ *     summary: Get a role
+ *     tags: [Roles]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: uuidv4
+ *         description: Role id
+ *     responses:
+ *       "302":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Role'
+ *       "500":
+ *         $ref: '#/components/responses/Forbidden'
+ */
