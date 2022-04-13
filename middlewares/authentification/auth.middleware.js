d5f5f7e6b84ebd68d6a70985fe475e5f2646f08a
StanDelamourd <sdelamourd@gmail.com>
2022-04-13 19:19:42 +0200

    add authentification middleware

--- middlewares/authentification/auth.middleware.js
+++ middlewares/authentification/auth.middleware.js
@@ -1,7 +1,40 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = {
-    authenticate: (req, res, next) => {
-        
+    authenticatePublicJWT: (req, res, next) => {
+        const authHeader = req.headers.authorization;
+
+        if (authHeader) {
+            const token = authHeader.split(' ')[1];
+
+            jwt.verify(token, process.env.SECRET_TOKEN_API, (err, user) => {
+                if (err) {
+                    return res.sendStatus(403);
+                }
+
+                req.user = user;
+                next();
+            });
+        } else {
+            res.sendStatus(401);
+        }
+    },
+    authenticateAdminJWT: (req, res, next) => {
+        const authHeader = req.headers.authorization;
+
+        if (authHeader) {
+            const token = authHeader.split(' ')[1];
+
+            jwt.verify(token, process.env.SECRET_TOKEN, (err, user) => {
+                if (err) {
+                    return res.sendStatus(403);
+                }
+
+                req.user = user;
+                next();
+            });
+        } else {
+            res.sendStatus(401);
+        }
     }
 }
