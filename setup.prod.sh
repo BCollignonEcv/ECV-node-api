b94233bd427208c8788c3c86ab2ccd4ce06d0eb5
BCollignonEcv <baptiste.collignon@mail-ecv.fr>
2022-04-14 03:09:43 +0200

    add production configuration

--- setup.prod.sh
+++ setup.prod.sh
@@ -2,11 +2,33 @@
 set -e
 
 # Install dependencies
-echo npm:install
+echo -------------------
+echo --- npm:install ---
+echo -------------------
+echo
 npm install
 
 # Migrate Sqlite database
-echo db:migrate
+echo
+echo --------------------
+echo ---- db:migrate ----
+echo --------------------
+echo
 npx sequelize-cli db:migrate
 
-cd -
+# Add data to database
+echo
+echo -------------------
+echo ----- db:seed -----
+echo -------------------
+echo
+npx sequelize-cli db:seed --seed 20220409-default-data.dev.js
+
+# Run API
+echo
+echo -------------------
+echo ----- run:API -----
+echo -------------------
+echo
+
+npm run start
